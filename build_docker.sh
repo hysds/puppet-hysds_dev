e22e74dcbceb8a3671556a59a8ebca1a006fbfbd
gmanipon <gmanipon@jpl.nasa.gov>
2018-09-24 06:24:47 +0000

    no cache

--- build_docker.sh
+++ build_docker.sh
@@ -7,4 +7,4 @@ if [ "$#" -ne 1 ]; then
 fi
 TAG=$1
 
-docker build --rm --force-rm -t hysds/dev:${TAG} -f docker/Dockerfile .
+docker build --no-cache --rm --force-rm -t hysds/dev:${TAG} -f docker/Dockerfile .
