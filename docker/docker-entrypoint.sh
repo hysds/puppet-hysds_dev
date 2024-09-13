d18d0ad78a65c54cce761d7a2b801d2b6284d968
Mike Cayanan <michael.d.cayanan@jpl.nasa.gov>
2024-09-13 11:08:48 -0700

    remove ops user entirely

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -8,13 +8,13 @@ export HOME=/root
 GID=$(id -g)
 
 # update user and group ids
-gosu 0:0 groupmod -g $GID ops 2>/dev/null
-gosu 0:0 usermod -u $UID -g $GID ops 2>/dev/null
-gosu 0:0 usermod -aG docker ops 2>/dev/null
+#gosu 0:0 groupmod -g $GID ops 2>/dev/null
+#gosu 0:0 usermod -u $UID -g $GID ops 2>/dev/null
+#gosu 0:0 usermod -aG docker ops 2>/dev/null
 
 # update ownership
-gosu 0:0 chown -R $UID:$GID $HOME 2>/dev/null || true
-gosu 0:0 chown -R $UID:$GID /var/run/docker.sock 2>/dev/null || true
+#gosu 0:0 chown -R $UID:$GID $HOME 2>/dev/null || true
+#gosu 0:0 chown -R $UID:$GID /var/run/docker.sock 2>/dev/null || true
 
 # source bash profile
 source $HOME/.bash_profile
