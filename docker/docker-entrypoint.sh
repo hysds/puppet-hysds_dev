2285990111a3315cc34cbfe95f27c4fea7471616
Michael Cayanan <42812746+mcayanan@users.noreply.github.com>
2024-10-14 08:45:43 -0700

    HC-522-update: Updates to support moving away from the OPS user in the Core containers (#6)
    * add kludges for testing
    
    * set HOME to root
    
    * remove ops user entirely
    
    * remove commented out code
    
    ---------
    
    Co-authored-by: Mike Cayanan <michael.d.cayanan@jpl.nasa.gov>

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -2,19 +2,14 @@
 set -e
 
 # set HOME explicitly
-export HOME=/home/ops
+export HOME=/root
 
 # get group id
 GID=$(id -g)
 
-# update user and group ids
-gosu 0:0 groupmod -g $GID ops 2>/dev/null
-gosu 0:0 usermod -u $UID -g $GID ops 2>/dev/null
-gosu 0:0 usermod -aG docker ops 2>/dev/null
-
-# update ownership
-gosu 0:0 chown -R $UID:$GID $HOME 2>/dev/null || true
-gosu 0:0 chown -R $UID:$GID /var/run/docker.sock 2>/dev/null || true
+if [ -e /var/run/docker.sock ]; then
+  gosu 0:0 chown -R $UID:$GID /var/run/docker.sock 2>/dev/null || true
+fi
 
 # source bash profile
 source $HOME/.bash_profile
